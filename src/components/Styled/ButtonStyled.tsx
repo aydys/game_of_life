1f09e490b9086711da9774379e63713a204416cd
Aidys Dongak <aidongak23@gmail.com>
2021-03-09 09:13:40 +0700

    add button styled

---
+++ src/components/Styled/ButtonStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const ButtonStyled = styled.button`
+  padding: 10px;
+  background-color: #4EA5D9;
+  font-size: 16px;
+  border-radius: 4px;
+  color: ;
+  font-weight: bold;
+  &:hover {
+    color: white;
+  }
+`
