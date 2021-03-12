5e09233fa6c3352bffee5ec60d00b4c8c7574519
Aidys Dongak <aidongak23@gmail.com>
2021-03-12 08:42:50 +0700

    add babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-react",
-    "@babel/preset-typescript",
+    "@babel/preset-typescript"    
   ],
+  plugins: [
+    "@babel/plugin-proposal-optional-chaining"
+  ]
 };
